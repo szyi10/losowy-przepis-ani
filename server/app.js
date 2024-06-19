f99807f0475e2884edb2a3803ccc6f79338034e1
szyi <szymon.ked16@gmail.com>
2024-06-19 12:40:46 +0200

    fixed bug

--- server/app.js
+++ server/app.js
@@ -1,48 +1,38 @@
 const express = require("express")
-const cors = require("cors")
+const cron = require("node-cron")
 const fs = require("fs")
-const schedule = require("node-schedule")
-const path = require("path")
 
 const app = express()
-app.use(cors())
+const PORT = process.env.PORT || 3000
 
-let recipes = []
 let selectedRecipe = {}
 
 // Load recipes from JSON file
-fs.readFile(path.join(__dirname, "recipes.json"), "utf8", (err, data) => {
-  if (err) {
-    console.error("Error reading recipes.json:", err)
-    return
-  }
-  recipes = JSON.parse(data)
-})
-
-// Function to select a random recipe
-function selectRandomRecipe() {
-  if (recipes.length > 0) {
-    selectedRecipe = recipes[Math.floor(Math.random() * recipes.length)]
-  }
+const loadRecipes = () => {
+  const data = fs.readFileSync("recipes.json")
+  return JSON.parse(data)
 }
 
-// Schedule job to run daily at 6:30 AM EST
-const est = "America/New_York"
-schedule.scheduleJob({ hour: 6, minute: 30, tz: est }, selectRandomRecipe)
+// Pick random recipe
+const pickRandomRecipe = () => {
+  const recipes = loadRecipes()
+  const randomIndex = Math.floor(Math.random() * recipes.length)
+  selectedRecipe = recipes[randomIndex]
+}
 
-// Endpoint to get the randomly selected recipe
-app.get("/random_recipe", (req, res) => {
+// Schedule task
+cron.schedule("45 12 * * *", () => {
+  pickRandomRecipe()
+  console.log("New random recipe selected: ", selectedRecipe)
+})
+
+// Initial pick of the recipe
+pickRandomRecipe()
+
+app.get("/random-recipe", (req, res) => {
   res.json(selectedRecipe)
 })
 
-// Endpoint to manually trigger selecting a random recipe
-app.get("/trigger_random_recipe", (req, res) => {
-  selectRandomRecipe()
-  res.json(selectedRecipe)
-})
-
-// Start the server
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
