585763c29be2628c5ac95dd6a56b14914bfc1b12
szyi <szymon.ked16@gmail.com>
2024-06-20 13:44:43 +0200

    firebase added

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { Recipe } from "../types"
 import recipes from "./recipes.json"
 
-const API_URL = "https://losowy-przepis-ani.onrender.com"
+// const API_URL = "https://losowy-przepis-ani.onrender.com"
 
 export const getRandomRecipe = async (): Promise<Recipe | null> => {
   const randomIndex = Math.floor(Math.random() * recipes.length)
@@ -13,7 +13,7 @@ export const getDailyRecipe = async (): Promise<Recipe | null> => {
   try {
     const res = await axios({
       method: "GET",
-      url: `${API_URL}/random_recipe`,
+      url: `https://losowy-przepis-81a65-default-rtdb.europe-west1.firebasedatabase.app/selectedRecipe.json`,
     })
 
     if (res.status === 200) {
